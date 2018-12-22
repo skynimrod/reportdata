849ee71d373d655bbdc83ac7a825d91d47d15309
adams <adamswang_2000@aliyun.com>
2018-12-22 10:47:36 +0800

    20181222

--- push.sh
+++ push.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 time=`date "+%Y%m%d"`
 echo "${time}"
+
+git add .;
+
 if [ -n "$1" ]; then  
     echo "commit $1!";
-    git add .;
     git commit -m "$1";
-    git push -u test master;
 else
     git commit -m "${time}";
-    git push -u test master;
 fi
+
+git push -u test master;
+
 echo "end!"
